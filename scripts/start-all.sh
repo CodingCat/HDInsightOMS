10514f1f08aeef55ab204e2ee99ccae4a9c00953
Nan Zhu <zhunansjtu@gmail.com>
2017-05-15 16:57:36 -0700

    fix upload destination directory

--- scripts/start-all.sh
+++ scripts/start-all.sh
@@ -6,7 +6,11 @@ workspaceid="91bc00a4-5b39-45a8-9a2e-f0a9ca9328c9"
 while IFS='' read -r line || [[ -n "$line" ]]; do
     slave_ip="$line"
 	ssh $username@$slave_ip sudo chown -R $username:$username /etc/opt/microsoft/omsagent/$workspaceid/conf/
-    scp -r conf/* $username@$slave_ip:/etc/opt/microsoft/omsagent/$workspaceid/conf/
+    scp -r conf/* $username@$slave_ip:/etc/opt/microsoft/omsagent/conf/omsagent.d
 	ssh $username@$slave_ip sudo /opt/microsoft/omsagent/ruby/bin/ruby /opt/microsoft/omsagent/bin/omsagent -d /var/opt/microsoft/omsagent/$workspaceid/run/omsagent.pid -o /var/opt/microsoft/omsagent/$workspaceid/log/omsagent.log -c /etc/opt/microsoft/omsagent/$workspaceid/conf/omsagent.conf --no-supervisor
 done < slaves
+
+
+
+
 
