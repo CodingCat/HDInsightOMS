2237f8f768955a9cb05b02b5627c9593fda216e9
Nan Zhu <zhunansjtu@gmail.com>
2017-05-15 16:13:58 -0700

    fix reading

--- scripts/start-all.sh
+++ scripts/start-all.sh
@@ -3,7 +3,7 @@
 username="$1"
 workspaceid="91bc00a4-5b39-45a8-9a2e-f0a9ca9328c9"
 
-while read -r line; do
+while IFS='' read -r line || [[ -n "$line" ]]; do
     slave_ip="$line"
 	ssh $username@$slave_ip sudo chown -R $username:$username /etc/opt/microsoft/omsagent/$workspaceid/conf/
     scp -r conf/* $username@$slave_ip:/etc/opt/microsoft/omsagent/$workspaceid/conf/
