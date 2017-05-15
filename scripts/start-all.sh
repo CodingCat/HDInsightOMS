546bf70161cba482386bc75e8e06b9da23860f2c
Nan Zhu <zhunansjtu@gmail.com>
2017-05-15 15:20:48 -0700

    hello

--- scripts/start-all.sh
+++ scripts/start-all.sh
@@ -2,12 +2,12 @@
 
 filename="$1"
 username="$2"
-workspaceid="$3"
+workspaceid="91bc00a4-5b39-45a8-9a2e-f0a9ca9328c9"
 
 while read -r line
 do
     slave_ip="$line"
-	ssh "@username"@"@slave_ip" "sudo chown -R $username:$username /etc/opt/microsoft/omsagent/$workspaceid/conf/"
-    #scp -r conf/* "@username"@"@slave_ip":/etc/opt/microsoft/omsagent/"$workspaceid"/conf/
+	ssh "$username"@"$slave_ip" "sudo chown -R $username:$username /etc/opt/microsoft/omsagent/$workspaceid/conf/"
+    scp -r conf/* "@username"@"@slave_ip":/etc/opt/microsoft/omsagent/"$workspaceid"/conf/
 done < "$filename"
 
