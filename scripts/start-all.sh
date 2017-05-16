3b723b86940c8da93221d8df653a7cef1b0d4ded
Nan Zhu <zhunansjtu@gmail.com>
2017-05-16 09:58:09 -0700

    add kill process command

--- scripts/start-all.sh
+++ scripts/start-all.sh
@@ -11,6 +11,7 @@ for slave_ip in `echo $HOSTLIST|sed  "s/#.*$//;/^$/d"`; do
     echo $slave_ip
     ssh $username@$slave_ip sudo chown -R $username:$username /etc/opt/microsoft/omsagent/$workspaceid/conf/
     scp -r conf/* $username@$slave_ip:/etc/opt/microsoft/omsagent/conf/omsagent.d
+	ssh $username@$slave_ip sudo for pid in $(ps -ef | grep "omsagent" | awk '{print $2}'); do kill -9 $pid; done
 	ssh $username@$slave_ip sudo /opt/microsoft/omsagent/ruby/bin/ruby /opt/microsoft/omsagent/bin/omsagent -d /var/opt/microsoft/omsagent/$workspaceid/run/omsagent.pid -o /var/opt/microsoft/omsagent/$workspaceid/log/omsagent.log -c /etc/opt/microsoft/omsagent/$workspaceid/conf/omsagent.conf --no-supervisor
 done 
 
